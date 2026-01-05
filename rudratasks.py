5b54826f4b9afb26a92e24368684c4e18ed520c3
Rudra <yadavrudra00010@gmail.com>
2026-01-05 22:15:54 +0530

    Enhance task input and add welcome messages
    Improved user input handling and added welcome messages.

--- rudratasks.py
+++ rudratasks.py
@@ -1,13 +1,25 @@
+# RudraTasks🎯
+# A simple To-Do-List app by Rudra
+# Version 1.0- January 2026
 
 tasks=["learn enumerate","solve maths","do homework","call a friend"]
+print("=== Welcome to Rudra Tasks 🚀 ===")
 print("To-Do-List:")
 while True:
-    user_input=input("enter the tasks: (or type 'done' to quit):")
+    user_input=input("enter the tasks :") .strip()
     if user_input.lower()=="done":
         print("quit")
         break
-    tasks.append(user_input)
+    if user_input:
+     tasks.append(user_input)
+     print(f"Added ✅  :  {user_input}")
+    else:
+        print("   ⚠️ Empty Task Skipped!")
     
     
 for index,task in enumerate(tasks, start=1):
      print(f"{index}.[ ] {task}")
+
+print("Keep grinding💪")
+print("Built by Rudra in 2026🎯")
+
